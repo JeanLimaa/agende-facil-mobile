12c1921db411f74a7462ab97200a201828ea218c
Jean Lima <jeansantoslima17@gmail.com>
2025-01-31 15:24:04 -0300

    feat: update color constants to use mainColor for light theme

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -9,7 +9,8 @@ const tintColorDark = '#fff';
 export const Colors = {
   light: {
     text: '#11181C',
-    background: '#fff',
+    mainColor: '#F97316',
+    background: '#F97316',
     tint: tintColorLight,
     icon: '#687076',
     tabIconDefault: '#687076',
