edd817314c578d9e37d02a646bda8d3a2fabecef
Jean Lima <jeansantoslima17@gmail.com>
2025-01-31 15:24:15 -0300

    feat: implement HomeScreen with login functionality and add ForgotPassword component

---
+++ app/auth/forgot.password.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function ForgotPassword(){
+    return (
+        <View>
+            <Text>Esqueci minha senha</Text>
+        </View>
+    )
+}
