5fb56318758cc57d8cfe516bb9ec751987cb7874
Jean Lima <jeansantoslima17@gmail.com>
2025-01-31 15:23:34 -0300

    refactor: navigation and authentication logic; remove unused tab layout and explore screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,37 +3,53 @@ import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
 import 'react-native-reanimated';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { router } from 'expo-router';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
   useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
+    const checkLoginStatus = async () => {
+      const token = await AsyncStorage.getItem('auth_token');
+      console.log(token);
+      if (token) {
+        router.push('/(tabs)');
+        setIsLoggedIn(true);
+      } else {
+        router.push('/auth');
+        setIsLoggedIn(false);
+      }
+      if (loaded) {
+        SplashScreen.hideAsync();
+      }
+    };
+
+    checkLoginStatus();
+  }, []);
 
   if (!loaded) {
     return null;
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+          {isLoggedIn ? (
+              <Stack.Screen name="(tabs)/index" options={{ headerShown: false }} />
+          ) : (
+              <Stack.Screen name="auth/index" options={{ headerShown: false }}  />
+          )}
+          <Stack.Screen name="auth/login" />
+          <Stack.Screen name="+not-found" />
       </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
   );
 }
