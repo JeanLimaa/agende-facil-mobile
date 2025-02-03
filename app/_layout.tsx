836eeee7be382ee87377d545413c56ece14df395
Jean Lima <jeansantoslima17@gmail.com>
2025-02-03 14:54:15 -0300

    feat: refactor RootLayout to use AuthProvider and simplify navigation logic

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,55 +1,19 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect, useState } from 'react';
-import { useNavigation } from '@react-navigation/native';
 import 'react-native-reanimated';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { router } from 'expo-router';
+import React from 'react';
+import { AuthProvider } from '@/modules/auth/contexts/AuthContext';
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
-
-  useEffect(() => {
-    const checkLoginStatus = async () => {
-      const token = await AsyncStorage.getItem('auth_token');
-      console.log(token);
-      if (token) {
-        router.push('/(tabs)');
-        setIsLoggedIn(true);
-      } else {
-        router.push('/auth');
-        setIsLoggedIn(false);
-      }
-      if (loaded) {
-        SplashScreen.hideAsync();
-      }
-    };
-
-    checkLoginStatus();
-  }, []);
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
-      <Stack>
-          {isLoggedIn ? (
-              <Stack.Screen name="(tabs)/index" options={{ headerShown: false }} />
-          ) : (
-              <Stack.Screen name="auth/index" options={{ headerShown: false }}  />
-          )}
-          <Stack.Screen name="auth/login" />
+    <AuthProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" />
+          <Stack.Screen name="auth" />
           <Stack.Screen name="+not-found" />
       </Stack>
+    </AuthProvider>
   );
 }
