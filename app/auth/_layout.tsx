5a0d6c1251193d19a2252f27a450cc8893648733
Jean Lima <jeansantoslima17@gmail.com>
2025-02-04 11:24:45 -0300

    feat: simplify Stack component by consolidating screen options for authentication routes

--- app/auth/_layout.tsx
+++ app/auth/_layout.tsx
@@ -3,10 +3,10 @@ import { Redirect, Stack } from "expo-router";
 
 function AuthStack(){
     return (
-        <Stack>
-            <Stack.Screen name="login" options={{ headerShown: false }} />
-            <Stack.Screen name="register" options={{ headerShown: false }} />
-            <Stack.Screen name="forgot-password" options={{ headerShown: false }} />
+        <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="login" />
+            <Stack.Screen name="register" />
+            <Stack.Screen name="forgot-password" />
         </Stack>
     )
 }
