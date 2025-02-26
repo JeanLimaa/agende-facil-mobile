3e3238cf2e801ea557c14a69bbf6456d2ac8f720
Jean Lima <jeansantoslima17@gmail.com>
2025-02-26 12:14:31 -0300

    feat: update BASE_URL to support dynamic development environment configuration

--- constants/apiUrl.ts
+++ constants/apiUrl.ts
@@ -1,1 +1,8 @@
-export const BASE_URL = process.env.EXPO_PUBLIC_API_URL || "http://localhost:3000";
+import Constants from 'expo-constants';
+
+const isDevelopmentMode = process.env.NODE_ENV === "development";
+const urlDevelopment = `http://${Constants.expoConfig?.hostUri?.split(':')[0]}:3000`;
+
+export const BASE_URL = 
+    isDevelopmentMode ? urlDevelopment || process.env.EXPO_PUBLIC_API_URL
+    : process.env.EXPO_PUBLIC_API_URL || "http://localhost:3000";
